225a05aeb5578aa1923917eaec41d40590189acb
Koushikumbc <xv15653@umbc.edu>
2024-04-15 17:58:15 -0400

    Update script.sql

--- script_files/script.sql
+++ script_files/script.sql
@@ -9,18 +9,20 @@ BEGIN
     RETURN QUERY
     --creating connection between projects 
     WITH projects_relation AS(
-        SELECT a.preceding_project_id AS project_id, a.project_id AS follow_up,b.is_approved,b.is_viable,b.date_start,b.date_end,b.date_created
+        SELECT a.preceding_project_id AS project_id, a.project_id AS follow_up
         FROM projects a 
         INNER JOIN projects b 
         ON a.preceding_project_id = b.project_id
     )
 
     -- creating column with status which in future can be used to link projects to reports
-    WITH temp AS(
+    WITH project_status AS(
         SELECT project_id,
         CASE 
         WHEN is_approved=true THEN 'Approved_Project',
+
         WHEN is_approved=false THEN
+
             CASE WHEN date_start > dates_through THEN 'Out-Of-Range',
             
              WHEN date_start > todays_date THEN 'Future Project',
@@ -29,7 +31,17 @@ BEGIN
 
              WHEN project_id NOT IN (SELECT project_id FROM projects_relation) THEN 'Not Sold',
 
-             WHEN project_id IN (select project_id from projects_relation) and EXISTS(select follow_up from projects_relation a INNER JOIN projects b on a.follow_up = b.projects where b.date_start > todays_date) THEN 'SOLD',
+             WHEN project_id IN (select project_id from projects_relation) and EXISTS(select project_id from projects_relation a INNER JOIN projects b on a.follow_up = b.project_id where b.date_start > todays_date) THEN 'Sold',
+
+             WHEN project_id IN (select project_id from projects_relation) and EXISTS(select project_id from projects_relation a INNER JOIN projects b on a.follow_up = b.project_id where b.date_start < todays_date AND b.date_end >todays_date) 
+             THEN 'Work-In-Process',
+
+             WHEN project_id IN (select project_id from projects_relation) and EXISTS(select project_id from projects_relation a INNER JOIN projects b on a.follow_up = b.project_id WHERE b.is_approved = true) THEN 'Approved',
+
+             WHEN project_id IN (select project_id from projects_relation) and EXISTS(select project_id from projects_relation a INNER JOIN projects b on a.follow_up = b.project_id WHERE b.is_approved = false AND b.is_viable = true) THEN 'Not Approved',
+
+             WHEN project_id IN (select project_id from projects_relation) and EXISTS(select project_id from projects_relation a INNER JOIN projects b on a.follow_up = b.project_id WHERE b.is_approved = false AND b.is_viable = false) THEN 'Not Viable',
+
 
              WHEN (date_start BETWEEN as_of_date AND dates_through) AND (date_end BETWEEN as_of_date AND dates_through) AND (date_end > todays_date) THEN 'Current Project',
 
@@ -37,10 +49,23 @@ BEGIN
 
         END 'Unexpected Outcome' AS status
 
+        FROM projects
 
 
 
     ) 
-    SELECT p.preceding_project_id AS project_id, p.is_approved AS status, p.project_id AS follow_up
-    FROM projects p
-    where p.preceding_project_id IS NOT NULL AND 
+    
+    SELECT b.category_description as debug, a.project_id, a.status, c.follow_up
+    from project_status a
+    INNER JOIN reports b
+    ON a.status = b.category_name
+    LEFT JOIN projects_relation
+    ON a.project_id = c.project_id
+
+    END
+
+
+    select * from fallout_report('2024-01-01', '2024-06-30','2024-12-31')
+
+    --first run the tables script to copy the flat files then run this report
+    
